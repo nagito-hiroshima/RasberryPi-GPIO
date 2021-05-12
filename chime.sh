a013c4e1800ebee9d5a63f536fe37824f577912c
nagito-hiroshima <68215637+nagito-hiroshima@users.noreply.github.com>
2021-05-12 15:53:18 +0900

    Update chime.sh

--- chime.sh
+++ chime.sh
@@ -7,4 +7,4 @@ sudo echo out > /sys/class/gpio/gpio3/direction
 
 
 # 自動 
-(echo 1 > /sys/class/gpio/gpio26/value;sleep 5s;echo 0 > /sys/class/gpio/gpio26/value) 
+(echo 1 > /sys/class/gpio/gpio3/value;sleep 5s;cat /sys/class/gpio/gpio3/value;sleep 5s;echo 0 > /sys/class/gpio/gpio3/value) 
