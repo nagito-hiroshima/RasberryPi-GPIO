98b94d27755c8c1b8b6ed724cc0cfb2dafab9521
nagito-hiroshima <68215637+nagito-hiroshima@users.noreply.github.com>
2021-05-12 13:56:43 +0900

    Update chime.sh

--- chime.sh
+++ chime.sh
@@ -2,16 +2,16 @@
 # root権限昇格
 #sudo su -l
 # GPIO3を制御する
-sudo echo "26" > /sys/class/gpio/export
+sudo echo 37 > /sys/class/gpio/export
 
 # GPIO3を書き込み可能にする
-sudo echo "out" > /sys/class/gpio/gpio26/direction
+sudo echo out > /sys/class/gpio/gpio37/direction
 
 # GPIO3をOFFにする(LEDが消える)
-sudo echo 0 > /sys/class/gpio/gpio26/value
+sudo echo 0 > /sys/class/gpio/gpio37/value
 
 # GPIO3をONにする(LEDが点灯する)
-sudo echo 1 > /sys/class/gpio/gpio26/value
+sudo echo 1 > /sys/class/gpio/gpio37/value
 
 # 自動 
 #(echo 0 > /sys/class/gpio/gpio26/value;sleep 10ms;echo 1 > /sys/class/gpio/gpio26/value) | at 9:20
