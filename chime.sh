d650334c218a749ce0ba5f9f273466170ca233a1
nagito-hiroshima <68215637+nagito-hiroshima@users.noreply.github.com>
2021-05-14 15:21:59 +0900

    Update chime.sh

--- chime.sh
+++ chime.sh
@@ -3,8 +3,8 @@
 sudo echo 14 > /sys/class/gpio/export
 
 # GPIO3を書き込み可能にする
-sudo echo out > /sys/class/gpio/gpio14/direction
+sudo echo in > /sys/class/gpio/gpio14/direction
 
 
 # 自動 
-(echo 1 > /sys/class/gpio/gpio14/value;sleep 5s;cat /sys/class/gpio/gpio14/value;sleep 5s;echo 0 > /sys/class/gpio/gpio14/value) 
+(echo 0 > /sys/class/gpio/gpio14/value;sleep 5s;cat /sys/class/gpio/gpio14/value;sleep 5s;echo 1 > /sys/class/gpio/gpio14/value) 
