a5a7a9421d4382738e39d93e8290ea67071e5945
nagito-hiroshima <68215637+nagito-hiroshima@users.noreply.github.com>
2021-05-14 15:24:09 +0900

    Update chime.sh

--- chime.sh
+++ chime.sh
@@ -3,16 +3,8 @@
 sudo echo 3 > /sys/class/gpio/export
 
 # GPIO3を書き込み可能にする
-<<<<<<< HEAD
-sudo echo in > /sys/class/gpio/gpio14/direction
-
-
-# 自動 
-(echo 0 > /sys/class/gpio/gpio14/value;sleep 5s;cat /sys/class/gpio/gpio14/value;sleep 5s;echo 1 > /sys/class/gpio/gpio14/value) 
-=======
 sudo echo out > /sys/class/gpio/gpio3/direction
 
 
 # 自動 
 (echo 1 > /sys/class/gpio/gpio3/value;sleep 5s;cat /sys/class/gpio/gpio3/value;sleep 5s;echo 0 > /sys/class/gpio/gpio3/value) 
->>>>>>> parent of a447783... aaaaa
