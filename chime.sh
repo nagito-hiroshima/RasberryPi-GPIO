a44778316cd514739de5cc3053271f40d3f9a9d7
nagito-hiroshima <68215637+nagito-hiroshima@users.noreply.github.com>
2021-05-14 15:14:56 +0900

    aaaaa

--- chime.sh
+++ chime.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # GPIO3を制御する
-sudo echo 3 > /sys/class/gpio/export
+sudo echo 14 > /sys/class/gpio/export
 
 # GPIO3を書き込み可能にする
-sudo echo out > /sys/class/gpio/gpio3/direction
+sudo echo out > /sys/class/gpio/gpio14/direction
 
 
 # 自動 
-(echo 1 > /sys/class/gpio/gpio3/value;sleep 5s;cat /sys/class/gpio/gpio3/value;sleep 5s;echo 0 > /sys/class/gpio/gpio3/value) 
+(echo 1 > /sys/class/gpio/gpio14/value;sleep 5s;cat /sys/class/gpio/gpio14/value;sleep 5s;echo 0 > /sys/class/gpio/gpio14/value) 
