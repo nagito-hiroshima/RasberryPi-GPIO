c5c7763ade5e78b47b33299546500988d8ae1a4c
nagito-hiroshima <68215637+nagito-hiroshima@users.noreply.github.com>
2021-05-19 20:13:01 +0900

    Update chime.sh

--- chime.sh
+++ chime.sh
@@ -20,4 +20,4 @@ echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/
 echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 16:05
 echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 16:15
 echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 17:30
-echo "echo 14 > /sys/class/gpio/export" | at 17:31
+echo "echo 14 > /sys/class/gpio/unexport" | at 17:31
