9d0d44d0d1cdc723982d788c32882c2c1f28883a
nagito-hiroshima <68215637+nagito-hiroshima@users.noreply.github.com>
2021-05-19 14:03:54 +0900

    Update chime.sh

--- chime.sh
+++ chime.sh
@@ -3,5 +3,5 @@
 # sudo echo 14 > /sys/class/gpio/export;
 
 # 自動 
-echo out > /sys/class/gpio/gpio14/direction | at 14:05
-echo in > /sys/class/gpio/gpio14/direction | at 14:06
+echo "out > /sys/class/gpio/gpio14/direction" | at 14:05
+echo "in > /sys/class/gpio/gpio14/direction"| at 14:06
