140c4896b281c9f165ae4deee98afb67a7087a3f
なぎと-nagito- <info@nagito.work>
2021-05-19 09:13:52 +0900

    Update chime.sh

--- chime.sh
+++ chime.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # GPIO3を制御する
+sudo echo 4 > /sys/class/gpio/export;
+echo out > /sys/class/gpio/gpio4/direction;
+echo 1 > /sys/class/gpio/gpio4/value;
 
 # 自動 
-(sudo echo 4 > /sys/class/gpio/export;echo out > /sys/class/gpio/gpio4/direction;sleep 5s;echo 4 > /sys/class/gpio/unexport) 
+(sudo echo 0 > /sys/class/gpio/gpio4/value;sleep 5s;echo 0 > /sys/class/gpio/gpio4/value) 
