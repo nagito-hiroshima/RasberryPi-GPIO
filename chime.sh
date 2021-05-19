19bfdef83e003206afb58c0b75d923aef7571f5a
nagito-hiroshima <68215637+nagito-hiroshima@users.noreply.github.com>
2021-05-19 14:38:15 +0900

    一応完成

--- chime.sh
+++ chime.sh
@@ -3,5 +3,19 @@
 # sudo echo 14 > /sys/class/gpio/export;
 
 # 自動 
-echo "out > /sys/class/gpio/gpio14/direction" | at 9:30
-echo "in > /sys/class/gpio/gpio14/direction" | at 9:45
+echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 14:36
+echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 9:30
+echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 9:45
+echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 10:35
+echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 10:45
+echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 11:35
+echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 11:45
+echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 12:35
+echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 13:15
+echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 14:05
+echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 14:15
+echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 15:05
+echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 15:15
+echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 16:05
+echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 16:15
+echo "echo out > /sys/class/gpio/gpio14/direction;sleep 2s;echo in > /sys/class/gpio/gpio14/direction" | at 17:30
